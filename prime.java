1a787cf5d668d666ed79d802c86b932c61fd8dbb
Srivaishnavi08 <153370530+Srivaishnavi08@users.noreply.github.com>
2024-11-28 10:38:07 +0530

    Update prime.java
    Changed n=8

--- prime.java
+++ prime.java
@@ -1,6 +1,6 @@
 public class prime{
     public static void main(String[] args) {
-        int n=9 ,c=0;
+        int n=8 ,c=0;
         for(int i=2;i<(n/2);i++){
             if(n%i==0){
                 System.out.println(n+" It is not prime");
