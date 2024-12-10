8af56bad082cf88224daa0608d89bfbec2487800
Srivaishnavi08 <153370530+Srivaishnavi08@users.noreply.github.com>
2024-12-10 21:54:22 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,17 +1,21 @@
 //this is sample java code
 public class prime{
-    public static void main(String[] args) {
-        int n=8 ,c=0;
+    public static boolean isPrime(int n)
+    {
         for(int i=2;i<(n/2);i++){
             if(n%i==0){
-                System.out.println(n+"Given n is not prime");
-                c=1;
-                break;
+                return false;
             }
         }
-        if(c==0){
-            System.out.println("Given num  is a prime");
+        return true;
+    }
+    public static void main(String[] args) {
+        for(int i=2;i<=100;i++)
+        {
+            if(isPrime(i))
+                System.out.println(i);
         }
+        
     }
 }
 //this is end of program
